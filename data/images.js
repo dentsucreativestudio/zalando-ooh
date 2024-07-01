888c564dc7248370365e9952a607f9e244007681
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-01 17:52:48 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,6 +1,6 @@
 let settings = [  
   { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando1.jpg'},
-  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando1.jpg'},
-  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando1.jpg'},
+  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando2.jpg'},
+  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando3.jpg'},
 ];
 
