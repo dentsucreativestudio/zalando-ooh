56eb56274ebc6fc482499fed76b7f7a0a6ec7a95
almeidaferreira <emailtohelder@gmail.com>
2024-07-01 16:29:15 +0200

    vsdgdfgkmfsdknmfds

---
+++ data/images.js
@@ -0,0 +1,6 @@
+var settings = [  
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
+  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
+];
+
