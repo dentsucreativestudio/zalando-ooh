447eea0e0713693d3eff80dccf685d2e14b9321b
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-01 17:48:12 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,6 +1,6 @@
 let settings = [  
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
-  { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
+  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando1.jpg'},
+  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando1.jpg'},
+  { image: 'https://raw.githubusercontent.com/dentsucreativestudio/zalando-ooh/main/images/zalando1.jpg'},
 ];
 
