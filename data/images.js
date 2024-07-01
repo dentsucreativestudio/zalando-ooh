6d4c0c735d2c2b393a3b5114023d0d994ce1f9a4
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-01 16:59:58 +0200

    Update images.js
    ff

--- data/images.js
+++ data/images.js
@@ -1,4 +1,4 @@
-var settings = [  
+let settings = [  
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Anna.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Eveline.jpg'},
   { image: 'https://dentsucreativestudio.github.io/canda-dooh/images/01-wijnegem/Brand_Xmas_Shooting_1080x1920px_Nancy.jpg'},
