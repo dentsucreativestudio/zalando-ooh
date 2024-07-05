0563560d5adb152fce22d50ef89ce25f8874e617
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-05 10:12:55 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,4 +1,5 @@
 let settings = [  
+  { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando5.jpg'},
   { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando4.jpg'},
   { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando3.jpg'},
   { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando2.jpg'},
