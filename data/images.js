ee90f9b2bad951e2ef079cea30f65102d7390b89
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-08 10:53:01 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,4 +1,4 @@
 let settings = [  
-  { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/ZAL0001_Festival_Les Ardentes_DOOH_Static_Jacques.jpg'},  
+  { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/ZAL0001_Festival_Les_Ardentes_DOOH_Static_Twins.jpg'},  
 ];
 
