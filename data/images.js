da6bdd8d62ddc6a2471842727e2a52999de6632e
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-11 20:33:04 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,4 +1,9 @@
 let settings = [  
-  { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/ZAL0001_Festival_Les Ardentes_DOOH_Static_Carl.jpg'},  
+  { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 1.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 12.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 13.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 14.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 15.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 16.jpg'}
+  { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/ZAL0001_Festival_Les Ardentes_DOOH_Static_Carl.jpg'}, 
 ];
-
