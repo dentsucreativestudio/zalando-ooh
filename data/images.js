36237166700178b4d8755f4f48092bca982238d4
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-11 20:50:05 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,9 +1,9 @@
 let settings = [  
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 1.jpg'}
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 12.jpg'}
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 13.jpg'}
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 14.jpg'}
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando dag 15.jpg'}
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_dag_16.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day1_1.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day1_2.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day1_3.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day1_4.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day1_5.jpg'}
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day1_6.jpg'}
   { image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/ZAL0001_Festival_Les Ardentes_DOOH_Static_Carl.jpg'}, 
 ];
