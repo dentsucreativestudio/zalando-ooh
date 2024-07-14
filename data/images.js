426c12f7eece8f6b26025c704124084a19d2749f
dentsucreativestudio <119412444+dentsucreativestudio@users.noreply.github.com>
2024-07-14 19:53:26 +0200

    Update images.js

--- data/images.js
+++ data/images.js
@@ -1,6 +1,14 @@
 var settings = [  
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day4_1.jpg'},
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day4_2.jpg'},
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day4_3.jpg'},
-{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_day4_4.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_1.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_2.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_3.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_4.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_5.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_6.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_7.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_8.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_9.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_10.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_11.jpg'},
+{ image: 'https://dentsucreativestudio.github.io/zalando-ooh/images/zalando_final_12.jpg'},
 ];
